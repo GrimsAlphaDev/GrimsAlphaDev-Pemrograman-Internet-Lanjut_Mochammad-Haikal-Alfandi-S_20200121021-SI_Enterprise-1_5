5ac09aa9275d5bcfbd943decedaa0694c38a38db
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-11-28 23:47:49 +0700

    Pertemuan 7_Search Data From Firestore Database 01

--- lib/app/modules/friends/views/friends_view.dart
+++ lib/app/modules/friends/views/friends_view.dart
@@ -1,17 +1,20 @@
 import 'package:antdesign_icons/antdesign_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pertama/app/data/controller/auth_controller.dart';
 import 'package:flutter_pertama/app/utils/style/AppColors.dart';
 import 'package:flutter_pertama/app/utils/widget/header.dart';
 import 'package:flutter_pertama/app/utils/widget/myFriends.dart';
 import 'package:flutter_pertama/app/utils/widget/sideBar.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:unicons/unicons.dart';
 
 import '../controllers/friends_controller.dart';
 
 class FriendsView extends GetView<FriendsController> {
   final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+  final authCon = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
@@ -35,58 +38,95 @@ class FriendsView extends GetView<FriendsController> {
                   !context.isPhone
                       ? const header()
                       : Container(
-                          // content / isi page / screen
-
                           padding: const EdgeInsets.all(20),
-                          child: Row(children: [
-                            IconButton(
-                              onPressed: () {
-                                _drawerKey.currentState!.openDrawer();
-                              },
-                              icon: const Icon(UniconsLine.bars),
-                              color: AppColors.primaryText,
-                            ),
-                            const SizedBox(
-                                // width: 15,
+                          child: Column(
+                            children: [
+                              Row(children: [
+                                IconButton(
+                                  onPressed: () {
+                                    _drawerKey.currentState!.openDrawer();
+                                  },
+                                  icon: const Icon(UniconsLine.bars),
+                                  color: AppColors.primaryText,
                                 ),
-                            Column(
-                              // mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  "Task Management",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      color: AppColors.primaryText),
+                                const SizedBox(
+                                    // width: 15,
+                                    ),
+                                Column(
+                                  // mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      "Task Management",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: AppColors.primaryText),
+                                    ),
+                                    Text(
+                                      "Manage Task Made Easy",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: AppColors.primaryText),
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  "Manage Task Made Easy",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: AppColors.primaryText),
+                                const Spacer(),
+                                const Icon(
+                                  AntIcons.bellFilled,
+                                  color: AppColors.primaryText,
+                                  size: 30,
                                 ),
-                              ],
-                            ),
-                            const Spacer(),
-                            const Icon(
-                              AntIcons.bellFilled,
-                              color: AppColors.primaryText,
-                              size: 30,
-                            ),
-                            const SizedBox(
-                                // width: 15,
+                                const SizedBox(
+                                    // width: 15,
+                                    ),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(30),
+                                  child: const CircleAvatar(
+                                    backgroundColor: Colors.amber,
+                                    radius: 20,
+                                    foregroundImage: NetworkImage(
+                                        'https://images.unsplash.com/photo-1620231150904-a86b9802656a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'),
+                                  ),
                                 ),
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(30),
-                              child: const CircleAvatar(
-                                backgroundColor: Colors.amber,
-                                radius: 20,
-                                foregroundImage: NetworkImage(
-                                    'https://images.unsplash.com/photo-1620231150904-a86b9802656a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'),
+                              ]),
+                              const SizedBox(
+                                height: 10,
                               ),
-                            ),
-                          ]),
+                              context.isPhone
+                                  ? TextField(
+                                      onChanged: (value) =>
+                                          authCon.searchFriends(value),
+                                      controller:
+                                          authCon.searchFriendController,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        contentPadding: const EdgeInsets.only(
+                                            left: 40, right: 10),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                              color: Colors.white),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                              color: Colors.blue),
+                                        ),
+                                        prefixIcon: const Icon(
+                                          Icons.search,
+                                          color: Colors.grey,
+                                        ),
+                                        hintText: 'Search',
+                                      ),
+                                    )
+                                  : const SizedBox(),
+                            ],
+                          ),
                         ),
+                  // content / isi page / screen
                   Expanded(
                     child: Container(
                       padding: !context.isPhone
@@ -101,71 +141,101 @@ class FriendsView extends GetView<FriendsController> {
                             ? BorderRadius.circular(50)
                             : BorderRadius.circular(30),
                       ),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              "People You May Know",
-                              style: TextStyle(
-                                  fontSize: 30, color: AppColors.primaryText),
-                            ),
-                            SizedBox(
-                              height: 200,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                shrinkWrap: true,
-                                clipBehavior: Clip.antiAlias,
-                                itemCount: 8,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Stack(
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(50),
-                                          child: const Image(
-                                            image: NetworkImage(
-                                                'https://images.unsplash.com/photo-1620231150904-a86b9802656a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'),
-                                          ),
-                                        ),
-                                        Positioned(
-                                          bottom: 10,
-                                          left: 55,
-                                          child: Text(
-                                            "George William",
-                                            style:
-                                                TextStyle(color: Colors.white),
-                                          ),
-                                        ),
-                                        Positioned(
-                                          bottom: 0,
-                                          right: 0,
-                                          child: SizedBox(
-                                            height: 36,
-                                            width: 36,
-                                            child: ElevatedButton(
-                                              onPressed: () {},
-                                              style: ElevatedButton.styleFrom(
-                                                padding: EdgeInsets.zero,
-                                                shape: RoundedRectangleBorder(
+                      child: Obx(
+                        () => authCon.hasilPencarian.isEmpty
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                    const Text(
+                                      "People You May Know",
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          color: AppColors.primaryText),
+                                    ),
+                                    SizedBox(
+                                      height: 200,
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        shrinkWrap: true,
+                                        clipBehavior: Clip.antiAlias,
+                                        itemCount: 8,
+                                        itemBuilder: (context, index) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(10.0),
+                                            child: Stack(
+                                              children: [
+                                                ClipRRect(
                                                   borderRadius:
                                                       BorderRadius.circular(50),
+                                                  child: const Image(
+                                                    image: NetworkImage(
+                                                        'https://images.unsplash.com/photo-1620231150904-a86b9802656a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'),
+                                                  ),
                                                 ),
-                                              ),
-                                              child: Icon(
-                                                  Icons.add_circle_outline),
+                                                const Positioned(
+                                                  bottom: 10,
+                                                  left: 55,
+                                                  child: Text(
+                                                    "George William",
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ),
+                                                Positioned(
+                                                  bottom: 0,
+                                                  right: 0,
+                                                  child: SizedBox(
+                                                    height: 36,
+                                                    width: 36,
+                                                    child: ElevatedButton(
+                                                      onPressed: () {},
+                                                      style: ElevatedButton
+                                                          .styleFrom(
+                                                        padding:
+                                                            EdgeInsets.zero,
+                                                        shape:
+                                                            RoundedRectangleBorder(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(50),
+                                                        ),
+                                                      ),
+                                                      child: const Icon(Icons
+                                                          .add_circle_outline),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                          ),
-                                        ),
-                                      ],
+                                          );
+                                        },
+                                      ),
                                     ),
-                                  );
-                                },
+                                    const MyFriends(),
+                                  ])
+                            : ListView.builder(
+                                padding: const EdgeInsets.only(
+                                    top: 10, bottom: 10, left: 0, right: 0),
+                                shrinkWrap: true,
+                                itemCount: authCon.hasilPencarian.length,
+                                itemBuilder: (context, index) => ListTile(
+                                  leading: ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image(
+                                      image: NetworkImage(
+                                          authCon.hasilPencarian[index]
+                                              ['photo'],
+                                          scale: 2),
+                                    ),
+                                  ),
+                                  title: Text(
+                                      authCon.hasilPencarian[index]['name']),
+                                  subtitle: Text(
+                                      authCon.hasilPencarian[index]['email']),
+                                  trailing: const Icon(Icons.add),
+                                ),
                               ),
-                            ),
-                            MyFriends(),
-                          ]),
+                      ),
                     ),
                   )
                 ],
